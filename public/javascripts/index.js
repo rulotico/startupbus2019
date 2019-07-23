4113d4700f7f8d3a32447a64602b5e6a05b0d031
rulotico <rulotico@gmail.com>
2019-07-23 01:59:40 -0500

    update: complete

---
+++ public/javascripts/index.js
@@ -0,0 +1,11 @@
+var maps=document.getElementsByClassName("map")
+
+console.log(maps)
+
+function show(){
+  maps[0].style.display="block";
+}
+
+function hide(){
+  maps[0].style.display="none";
+}
